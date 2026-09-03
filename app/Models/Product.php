d0bfba6cdc63dd4ab06260c52e9c3576c3a1cfdc
CarlosZetino36 <carlos.ruiz5@catolica.edu.sv>
2026-09-02 19:08:11 -0600

    Primera clase 20-08-26 avance: Eloquent

---
+++ app/Models/Product.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use App\Models\Category;
+use Illuminate\Database\Eloquent\Casts\Attribute;
+
+class Product extends Model
+{
+    protected $fillable = ['name', 'price', 'stock', 'category_id'];
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function scopeInStock($query)
+    {
+        return $query->where('stock', '>', 0);
+    }
+
+    protected function formattedPrice(): Attribute
+    {
+        return Attribute::make(
+            get: fn () => '$' . number_format($this->price, 2),
+        );
+    }
+
+
+}
