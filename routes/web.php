0b5884758728f3be6dd9e974367adba8afebec33
Monitoreo ACODES <monitoreo@acodes.onmicrosoft.com>
2026-09-03 16:55:04 -0600

    Creacion de rutas

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,22 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Models\Category;
+use App\Models\Product;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/reporte-cat-stock', function(){
+return[
+    'categorias_con_stock' => Category::with(['products' => fn($q) => $q->inStock()])->get(),
+];
+});
+
+
+Route::get('/products-by-category', function(){
+return[                     //funcion en mo
+    'reporte' => Product::productsByCategory(),
+];
 });
