58461bb4bd50bd5df9478f1ee8f166dd1092a682
CarlosZetino36 <carlos.ruiz5@catolica.edu.sv>
2026-09-03 18:01:24 -0600

    Store Request | new Producto Controller

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use App\Models\Category;
 use Illuminate\Database\Eloquent\Casts\Attribute;
+use Illuminate\Support\Facades\DB;
 
 class Product extends Model
 {
